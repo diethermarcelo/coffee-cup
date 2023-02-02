85349c874ce7e0baf6f2d1402fdb4a1dc77e2a39
Diether Marcelo <miguelitossss199@gmail.com>
2023-02-02 19:33:13 +0800

    Added features
    More mobile responsive
    Add header transition
    Add burger menu
    Add favicon

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+
+window.onscroll = function() {scrollFunction()};
+
+var navbar = document.querySelector("header");
+var sticky = navbar.offsetTop;
+
+function scrollFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+    navbar.classList.add("with-bg")
+  } else {
+    navbar.classList.remove("sticky");
+    navbar.classList.remove("with-bg")
+  }
+}
+
+// Burger
+
+let burger_close = document.querySelector('.burger-navs .close');
+let burger_icon = document.querySelector('.burger-container .burger-icon');
+let burger_navs = document.querySelector('.burger-navs');
+
+burger_close.addEventListener('click', () => {
+    burger_navs.classList.add('opacity-0');
+    burger_navs.classList.remove('opacity-1');
+    burger_navs.classList.add('important-index-negative-1');
+    burger_icon.classList.remove('important-index-0');
+})
+
+burger_icon.addEventListener('click', () => {
+    burger_navs.classList.add('opacity-1');
+    burger_navs.classList.remove('opacity-0');
+    burger_navs.classList.remove('important-index-negative-1');
+    burger_icon.classList.add('important-index-0');
+})
